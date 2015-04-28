d022af5e523dd5c0e891edb0aecee214d3ab8f63
Cristóbal Severino <cristobal.severino@gmail.com>
2015-04-28 17:35:57 -0300

    limpiando
    Limpieza a las librerías externas a dark lyrics

--- albums.py
+++ albums.py
@@ -5,10 +5,26 @@ import os
 import string
 
 
-def getAlbums(text):	
+def getAlbums(text):
 	for root, dirs, files in os.walk("."):
-	    path = root.split('/')	    
+	    path = root.split('/')
 	    for file in filter(lambda file: file.endswith('.txt'), files):
-	        print len(path)*'---', file
+        	getBand(open(os.path.join(root, file)).read())
+
+def getBand(archivo):
+	#print archivo	
+	soup = bs4.BeautifulSoup(archivo)
+
+	for a in soup.find_all('a', href=True):
+		link = a['href']
+
+		if not 'plyrics' in link:
+			if not 'media.fastclick' in link:
+				 if not 'azlyrics' in link:
+				 	if not 'oldielyrics' in link:
+						if not 'google' in link:
+							if not 'azvideos' in link:
+								print link
+
 
 getAlbums("lol")
